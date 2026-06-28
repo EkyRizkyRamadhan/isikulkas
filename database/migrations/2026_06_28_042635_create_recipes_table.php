bcaaa2548cea6ba1869d58ef2df03f55494e8f5b
EkyRizkyRamadhan <ekyramadhan1232@gmail.com>
2026-06-28 11:29:05 +0700

    feat: add recipes migration

---
+++ database/migrations/2026_06_28_042635_create_recipes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('ingredients_input'); // bahan yang diinput user (teks bebas)
+            $table->string('title');
+            $table->longText('content'); // isi resep lengkap dari Gemini (langkah2, dll)
+            $table->boolean('is_bookmarked')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
