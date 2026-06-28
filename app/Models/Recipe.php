0a0e2d9a3dc8d6966329bf7877129fae35913626
EkyRizkyRamadhan <ekyramadhan1232@gmail.com>
2026-06-28 11:34:43 +0700

    feat: implement Recipe model with relationships

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Recipe extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'ingredients_input', 'title', 'content', 'is_bookmarked',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
