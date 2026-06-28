f10aa167afd4317020786859320d83979edce53c
EkyRizkyRamadhan <ekyramadhan1232@gmail.com>
2026-06-28 11:41:32 +0700

    feat: add recipe routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.index');
+    Route::get('/recipes/create', [RecipeController::class, 'create'])->name('recipes.create');
+    Route::post('/recipes/generate', [RecipeController::class, 'generate'])->name('recipes.generate');
+    Route::get('/recipes/{recipe}', [RecipeController::class, 'show'])->name('recipes.show');
+    Route::patch('/recipes/{recipe}/bookmark', [RecipeController::class, 'bookmark'])->name('recipes.bookmark');
 });
 
 require __DIR__.'/auth.php';
